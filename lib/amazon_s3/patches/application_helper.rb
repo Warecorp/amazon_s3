d9d2fb4f02d1a5f4949e82f8d47e4dc2ef216f00
Sergey Yelenski <sergey.yelenski@warecorp.com>
2021-03-12 16:38:28 +0300

    plugin changed

--- lib/amazon_s3/patches/application_helper.rb
+++ lib/amazon_s3/patches/application_helper.rb
@@ -3,7 +3,6 @@ ApplicationHelper.class_eval do
     link_to image_tag(attachment.thumbnail_s3, data: {thumbnail: thumbnail_path(attachment)}),
       AmazonS3::Connection.object_url(attachment.disk_filename_s3),
       title: attachment.filename
-    )
   end
   
 end
